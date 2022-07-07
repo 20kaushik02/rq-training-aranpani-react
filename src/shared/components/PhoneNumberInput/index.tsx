83876d7420a274e29f08cf6922a3ffe28c737456
Kaushik Narayan R <kaushik.ravishankar@rootquotient.com>
2022-07-07 11:45:53 +0530

    phone number input component

---
+++ src/shared/components/PhoneNumberInput/index.tsx
@@ -0,0 +1,55 @@
+import { ErrorMessage, Field } from 'formik'
+import React, { FC } from 'react'
+import PhoneInput from 'react-phone-number-input'
+import "./phoneNumberInput.scss"
+import 'react-phone-number-input/style.css'
+
+interface PhoneNumberInputProps {
+    name: string
+    title?: string
+    disabled?: boolean
+    value: string | number | undefined
+    onChange: (mobile: string) => void
+    prefix?: JSX.Element | string
+}
+
+const PhoneNumberInput: FC<PhoneNumberInputProps> = (props) => {
+    const {
+        name,
+        value,
+        title,
+        disabled,
+        onChange,
+        prefix,
+    } = props
+    return (
+        <div className={`phone-number-input ${disabled && 'disabled-field'}`}>
+            {title && <label htmlFor={name}>{title}</label>}
+            <Field name={name}>
+                {({ field, meta }: any) =>
+                    <>
+                        <div className="phone-number-input-field">
+                            {prefix}
+                            <PhoneInput
+                                className='ant-input'
+                                placeholder="Enter"
+                                {...field}
+                                {...meta}
+                                disabled={disabled}
+                                countrySelectProps={{ unicodeFlags: true }}
+                                countryCallingCodeEditable={false}
+                                defaultCountry="IN"
+                                value={value as any}
+                                onChange={onChange} />
+                        </div>
+                        <span className='input__error'>
+                            <ErrorMessage className='input__error' name={name} />
+                        </span>
+                    </>
+                }
+            </Field>
+        </div>
+    )
+}
+
+export default PhoneNumberInput
